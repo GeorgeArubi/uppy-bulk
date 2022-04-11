3f82d9deea8ea0eeaf846f9f5752768d7e2da9b2
DOGHOUSETV <arubigeorge0@gmail.com>
2022-04-11 15:45:56 -0400

    Restructure aws-companion

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const Webcam = require('@uppy/webcam')
 const Dashboard = require('@uppy/dashboard')
 const AwsS3 = require('@uppy/aws-s3')
 
-const uppy  = new Uppy({
+const uppy = new Uppy({
+  debug: true,
   autoProceed: false,
-  restrictions: {
-    maxFileSize: 1000000000, // Uppy options, currently set max file size to 100MB
-    minNumberofFile: 1, // Set a max/min number of files
-    allowedFileTypes: ['audio/*'] // This restriction is currently set to accept any audio format (for specific file types use 'audio/wav', 'audio/mp3', etc.)
-  }  
+})
+
+uppy.use(GoogleDrive, {
+  companionUrl: 'http://localhost:3020',
 })
 uppy.use(Webcam)
 uppy.use(Dashboard, {
@@ -18,8 +18,6 @@ uppy.use(Dashboard, {
   target: 'body',
   plugins: ['GoogleDrive', 'Webcam'],
 })
-uppy.use(AwsS3, { 
+uppy.use(AwsS3, {
   companionUrl: 'http://localhost:3020',
-  metaFields: ['name', 'bulk-upload'] //  Root URL of the uppy-companion instance
-}) 
-
+})
--- server.js
+++ server.js
@@ -1,63 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const companion = require('@uppy/companion');
-const path = require('path');
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
 const fs = require('fs')
-const cors = require('cors')
+const path = require('path')
+const companion = require('../../packages/@uppy/companion')
+const app = require('express')()
 
-const app = express()
 const DATA_DIR = path.join(__dirname, 'tmp')
 
-app.use(cors({
-  origin: 'http://localhost:9966',
+app.use(require('cors')({
+  origin: true,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'OPTIONS', 'PATCH'],
 }))
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || 'http://localhost:9966')
-  next()
-})
-
-// Routes
-app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain')
-  res.send('Welcome to Companion')
-})
-
-app.use(cookieParser())
-app.use(bodyParser.json())
-app.use(session({
-  secret: 'some-secret-II',
-  resave: true,
-  saveUninitialized: true,
+app.use(require('cookie-parser')())
+app.use(require('body-parser').json())
+app.use(require('express-session')({
+  secret: 'hello planet',
 }))
 
 const options = {
   providerOptions: {
-    s3: {
-    getKey: (req, filename, metadata) => `uppy/${Math.random().toString(32).slice(2)}/${filename}`,
-    key: process.env.COMPANION_AWS_KEY,
-    secret: process.env.COMPANION_AWS_SECRET,
-    bucket: process.env.COMPANION_AWS_BUCKET,
-    region: process.env.COMPANION_AWS_REGION,
+    drive: {
+      key: process.env.COMPANION_GOOGLE_KEY,
+      secret: process.env.COMPANION_GOOGLE_SECRET,
     },
-    // you can also add options for additional providers here
+    s3: {
+      getKey: (req, filename) => `whatever/${Math.random().toString(32).slice(2)}/${filename}`,
+      key: process.env.COMPANION_AWS_KEY,
+      secret: process.env.COMPANION_AWS_SECRET,
+      bucket: process.env.COMPANION_AWS_BUCKET,
+      region: process.env.COMPANION_AWS_REGION,
+      endpoint: process.env.COMPANION_AWS_ENDPOINT,
+    },
   },
-  server: {
-      host: 'localhost:3020',
-      protocol: 'http', // 'http' || 'https'
-      path: '/companion',
-  },
+  server: { host: 'localhost:3020' },
   filePath: DATA_DIR,
-  secret: 'some-secret',
-  uploadUrls: 'http://localhost:9966',
+  secret: 'blah blah',
   debug: true,
-  allowLocalUrls: true, // Only enable this in development
-  acl: 'public-read',
-  maxFileSize: 1000000000,
 }
 
 // Create the data directory here for the sake of the example.
@@ -70,7 +46,7 @@ process.on('exit', () => {
   fs.rmSync(DATA_DIR, { recursive: true, force: true })
 })
 
-app.use('/companion', companion.app(options))
+app.use(companion.app(options))
 
 const server = app.listen(3020, () => {
   console.log('listening on port 3020')
